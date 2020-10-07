ae809e9e17efba7ea50d96e38bc83bec06e1742e
Chinami Kitagawa <cnm25.k@gmail.com>
2020-10-07 07:15:32 +0000

    add my_page branch

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_10_05_061045) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "item"
-    t.integer "amount"
+    t.string "amount"
     t.integer "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
