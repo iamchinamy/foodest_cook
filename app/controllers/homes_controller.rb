ad795c70104fb917b1cfecca2a2e23836c80f459
Chinami Kitagawa <cnm25.k@gmail.com>
2020-10-12 05:30:32 +0000

    change random to rand in top page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
 
 	def top
-		@random = Producer.order("RANDOM()").limit(3)
+		@random = Producer.order("RAND()").limit(3)
 		@all_ranks = Recipe.create_all_ranks
 	end
 
